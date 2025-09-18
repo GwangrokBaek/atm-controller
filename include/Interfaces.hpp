a6a1e5b2bf3e8d57cdd6cacb93c1ace41132b5a5
Gwangrok <phil@zigbang.com>
2025-09-19 00:54:21 +0900

    feat: add interface of bank

--- include/Interfaces.hpp
+++ include/Interfaces.hpp
@@ -1,12 +1,28 @@
 #pragma once
 #include <string>
+#include <vector>
 #include "Result.hpp"
 
 using namespace std;
 
 using Card = string;
+using Pin = string;
+using AccountId = string;
 
-class ICardReader{
+class IBank {
+public:
+    virtual ~IBank() = default;
+
+    virtual Status verifyPin(const Card& card, const Pin& pin) = 0;
+    virtual vector<AccountId> listAccounts(const Card& card) = 0;
+    virtual Result<int> getBalance(const AccountId& accountId) = 0;
+    
+    virtual Status deposit(const AccountId& accountId, int money) = 0;
+    virtual Status canWithdraw(const AccountId& accountId, int money) = 0;
+    virtual Status withdraw(const AccountId& AccountId, int money) = 0;
+};
+
+class ICardReader {
 public:
     virtual ~ICardReader() = default;
 
