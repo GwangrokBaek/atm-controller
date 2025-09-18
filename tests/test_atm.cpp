2a36dc044ac45fb17e9e7dc2c7263a1257c611e6
Gwangrok <phil@zigbang.com>
2025-09-18 22:26:03 +0900

    feat: add macro defined functions which logs current test step and results

--- tests/test_atm.cpp
+++ tests/test_atm.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+using namespace std;
+
+static int passed = 0;
+static int failed = 0;
+
+#if defined(_MCS_VER)
+#define FUNC_NAME __FUNCTION__
+#else
+#define FUNC_NAME __func__
+#endif
+
+#define TEST(name) \
+    void name(); \
+    void name() { \
+        cout << "[ RUN ] " << FUNC_NAME << endl; \
+
+#define END_TEST \
+    cout << "[ OK  ] " << FUNC_NAME << endl; \
+    cout << endl; \
+    }
+
+#define REQUIRE(cond) do { if(!(cond)) { cerr << "REQUIRE failed: " #cond "\n"; ++failed; } else ++passed; } while(0)
+
 int main()
 {
-    
+    cout << "Tests passed : " << passed << ", failed : " << failed << endl;
 }
