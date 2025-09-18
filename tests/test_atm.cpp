447180142c6264874e1b5aae97519684dba5bd97
Gwangrok <phil@zigbang.com>
2025-09-19 00:58:42 +0900

    feat: add test cases for bank feature

--- tests/test_atm.cpp
+++ tests/test_atm.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Controller.hpp"
 #include "FakeCardReader.hpp"
+#include "FakeBank.hpp"
 
 using namespace std;
 
@@ -26,12 +27,14 @@ static int failed = 0;
 #define REQUIRE(cond) do { if(!(cond)) { cerr << "REQUIRE failed: " #cond "\n"; ++failed; } else ++passed; } while(0)
 
 TEST(test_card_insert_and_eject)
-    Card card = "12345";
+    Card card = "CARD-001";
+
+    FakeBank bank({{"", ""}}, {{"", {}}}, {{},{}});
     FakeCardReader cardReader(card);
-    Controller atm(cardReader);
+    Controller atm(cardReader, bank);
 
     REQUIRE(atm.insertCard().isOk());
-    REQUIRE(cardReader.card.value() == "12345");
+    REQUIRE(cardReader.card.value() == card);
     REQUIRE(cardReader.inserted == true);
     REQUIRE(atm.state() == Controller::State::CardInserted);
     REQUIRE(atm.ejectCard().isOk());
@@ -39,9 +42,69 @@ TEST(test_card_insert_and_eject)
     REQUIRE(atm.state() == Controller::State::Idle);
 END_TEST
 
+TEST(test_withdraw_and_balance)
+    Card card = "CARD-001";
+    Pin pin = "12345";
+    AccountId account1 = "ACCOUNT-001";
+    AccountId account2 = "ACCOUNT-002";
+    int balance1 = 1000;
+    int balance2 = 1200;
+
+    unordered_map<Card, Pin> pinMap = {{card, pin}};
+    unordered_map<Card, vector<AccountId>> accountsMap = {{card, {account1, account2}}};
+    unordered_map<AccountId, int> balanceMap = {{account1, balance1}, {account2, balance2}};
+
+    FakeBank bank(pinMap, accountsMap, balanceMap);
+    FakeCardReader cardReader(card);
+    Controller atm(cardReader, bank);
+
+    REQUIRE(atm.insertCard().isOk());
+    REQUIRE(atm.enterPin(pin).isOk());
+    REQUIRE(atm.selectAccount(account1).isOk());
+    auto b1 = atm.getBalance();
+    REQUIRE(b1.isOk());
+    REQUIRE(atm.withdraw(120).isOk());
+    auto b2 = atm.getBalance();
+    REQUIRE(b2.isOk());
+    REQUIRE(b2.value() == b1.value() - 120);
+    REQUIRE(atm.ejectCard().isOk());
+    REQUIRE(atm.state() == Controller::State::Idle);
+END_TEST
+
+TEST(test_deposit_and_balance)
+    Card card = "CARD-001";
+    Pin pin = "12345";
+    AccountId account1 = "ACCOUNT-001";
+    AccountId account2 = "ACCOUNT-002";
+    int balance1 = 1000;
+    int balance2 = 1200;
+
+    unordered_map<Card, Pin> pinMap = {{card, pin}};
+    unordered_map<Card, vector<AccountId>> accountsMap = {{card, {account1, account2}}};
+    unordered_map<AccountId, int> balanceMap = {{account1, balance1}, {account2, balance2}};
+
+    FakeBank bank(pinMap, accountsMap, balanceMap);
+    FakeCardReader cardReader(card);
+    Controller atm(cardReader, bank);
+
+    REQUIRE(atm.insertCard().isOk());
+    REQUIRE(atm.enterPin(pin).isOk());
+    REQUIRE(atm.selectAccount(account1).isOk());
+    auto b1 = atm.getBalance();
+    REQUIRE(b1.isOk());
+    REQUIRE(atm.deposit(250).isOk());
+    auto b2 = atm.getBalance();
+    REQUIRE(b2.isOk());
+    REQUIRE(b2.value() == b1.value() + 250);
+    REQUIRE(atm.ejectCard().isOk());
+    REQUIRE(atm.state() == Controller::State::Idle);
+END_TEST
+
 int main()
 {
     test_card_insert_and_eject();
+    test_withdraw_and_balance();
+    test_deposit_and_balance();
 
     cout << "Tests passed : " << passed << ", failed : " << failed << endl;
 }
