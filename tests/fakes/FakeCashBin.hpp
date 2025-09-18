e98d646a666c75c812cef0bfca460a3d47c9fe6b
Gwangrok <phil@zigbang.com>
2025-09-19 01:17:06 +0900

    feature: add fake cashbin class for test

---
+++ tests/fakes/FakeCashBin.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include "Interfaces.hpp"
+
+class FakeCashBin : public ICashBin {
+public:
+    int capacity;
+
+    FakeCashBin(int capacity) : capacity(capacity)
+    {}
+
+    Status canDispense(int money)
+    {
+        return (money <= capacity) ? Status::okStatus() : Status::error(Err::InsufficientCashBin);
+    }
+
+    Status dispense(int money)
+    {
+        if (money <= capacity)
+        {
+            capacity -= money;
+            return Status::okStatus();
+        }
+        return Status::error(Err::InsufficientCashBin);
+    }
+};
