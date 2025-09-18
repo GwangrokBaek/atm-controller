de9381b19de4021c71100efd1e14e2936ccd376c
Gwangrok <phil@zigbang.com>
2025-09-19 00:55:12 +0900

    feat: add bank related feature to controller class

--- include/Controller.hpp
+++ include/Controller.hpp
@@ -8,20 +8,31 @@ class Controller {
 public:
     enum class State { Idle, CardInserted, Authenticated, AccountSelected };
 
+    struct Config {
+        int maxPinAttempts = 3;
+    };
+
 private:
     State _state = State::Idle;
     ICardReader& _cardReader;
+    IBank& _bank;
+
+    Config _cfg;
 
     optional<Card> _card;
+    optional<AccountId> _account;
 
     int _pinAttempts;
 
-    // TODO: Bank
+    static bool validMoney(int money)
+    {
+        return money >= 0;
+    }
 
     // TODO: Cash Bin
 
 public:
-    Controller(ICardReader& cardReader) : _cardReader(cardReader)
+    Controller(ICardReader& cardReader, IBank& bank) : _cardReader(cardReader), _bank(bank)
     {}
 
     State state(void) const;
@@ -30,7 +41,13 @@ public:
     Status insertCard(void);
     Status ejectCard(void);
 
-    // TODO: Bank
+    // Bank
+    Status enterPin(const Pin& pin);
+    Result<vector<AccountId>> listAccounts() const;
+    Status selectAccount(const AccountId& accountId);
+    Result<int> getBalance(void) const;
+    Status deposit(int money);
+    Status withdraw(int money);
 
     // TODO: Cash Bin
 };
